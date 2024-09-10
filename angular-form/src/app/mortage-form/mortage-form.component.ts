859868704aa15af991dd06e2297a296e02c0ec8a
narendranmurali2001 <narendranmurali2001@gmail.com>
2024-09-10 12:31:08 +0000

    boxed

--- angular-form/src/app/mortage-form/mortage-form.component.ts
+++ angular-form/src/app/mortage-form/mortage-form.component.ts
@@ -1,21 +1,43 @@
-import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { NgClass, NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-mortage-form',
   standalone: true,
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule, NgClass, NgIf],
   templateUrl: './mortage-form.component.html',
   styleUrl: './mortage-form.component.css'
 })
 export class MortageFormComponent {
 
-  mortageForm = new FormGroup({
+  repaymentAmount: string = '';
+  totalRepayment: string = '';
 
+  mortageForm = new FormGroup({
+    mortageAmount: new FormControl(10000, [Validators.required, Validators.min(1000)]),
+    mortageTerm: new FormControl(15, [Validators.required, Validators.min(10)]),
+    interestRate: new FormControl(3, [Validators.required, Validators.min(3), Validators.max(7)]),
   })
 
   get f() {
     return this.mortageForm.controls;
+  }
+
+  calculateRepayments() {
+    const { mortageAmount, mortageTerm, interestRate } = this.mortageForm.value;
+    if (mortageAmount !== null && mortageTerm !== null && interestRate !== null) {
+      const monthlyInterest = (interestRate as number) / 100 / 12;
+      const monthlyPayment = (mortageAmount as number) * monthlyInterest / (1 - Math.pow(1 + monthlyInterest, -(mortageTerm as number) * 12));
+
+      this.repaymentAmount = monthlyPayment.toFixed(2);
+      this.totalRepayment = (monthlyPayment * (mortageTerm as number) * 12).toFixed(2);
+    }
+  }
+
+  resetForm() {
+    this.mortageForm.reset();
+    this.repaymentAmount = '';
+    this.totalRepayment = '';
   }
 }
